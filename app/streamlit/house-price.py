2c2178e5650096097aeb317c856c628e3eac96ea
Josia Given Santoso <josgiv1542@gmail.com>
2024-06-28 21:58:15 +0700

    house Price

--- app/streamlit/house-price.py
+++ app/streamlit/house-price.py
@@ -4,7 +4,8 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 
 # Load Dataset
-df = pd.read_csv('..\..\ML-preparation\Dataset\Harga-Rumah-Model.csv', delimiter=',')
+df = pd.read_csv('..\\..\\ML-preparation\\Dataset\\Harga-Rumah-Model.csv', delimiter=',')
+
 
 # Mapping sub-lokasi berdasarkan kota
 sub_lokasi_dict = {
