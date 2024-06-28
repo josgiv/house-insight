3b347b968750e4bf1f636126cbdc419c2af35840
Josia Given Santoso <josgiv1542@gmail.com>
2024-06-28 22:08:38 +0700

    House Price Fix

--- app/streamlit/house-price.py
+++ app/streamlit/house-price.py
@@ -4,18 +4,13 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 
 import os
-
-file_path = os.path.abspath('Harga-Rumah-Model.csv')
-df = file_path
-
-
 import streamlit as st
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 import os
 
-file_path = os.path.abspath('../../ML-preparation/Dataset/Harga-Rumah-Model.csv')
+file_path = os.path.abspath('Harga-Rumah-Model.csv')
 df = pd.read_csv(file_path)  # Read the CSV file into a dataframe
 
 # Mapping sub-lokasi berdasarkan kota
